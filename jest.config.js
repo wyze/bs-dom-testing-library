0a581d8377b735d30fbcc53fcd1b4037f1713703
Stephanie Carr <8503736+sacarr@users.noreply.github.com>
2021-11-03 15:37:09 -0400

    Migrate from bs-platform to rescript

--- jest.config.js
+++ jest.config.js
@@ -140,9 +140,9 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [
-  //   "./__tests__/setupTests.js"
-  // ],
+  setupFilesAfterEnv: [
+    "./src/__tests__/setupTests.js"
+  ],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
